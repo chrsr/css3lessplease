d4239cb40c522bb4108428fdc736e3813512611a
Chris Roberts <chris@chrsr.com>
2012-11-17 15:40:35 +1000

    Example less conversion

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
-var css3lessplease = {};
+
+/* globals css3lessplease, less */
+
+var css3lessplease = (function () {
+    var parser = new(less.Parser),
+        selectors = {
+            compile: ".compile"
+        }
+
+    function lessParser (less, output) {
+        var options = {
+            removeComments: true // do this
+        }
+        parser.parse(less, function (err, tree) {
+            if (err) {
+                output.html(err);
+                return;
+            }
+            output.html(tree.toCSS());
+        });
+    }
+
+    return {   
+        init: function() {
+            // $(".code pre").html($(".code span").html().trim());
+
+            $(selectors.compile).on("click", function () {
+                var less,
+                    output,
+                    $this = $(this).parents(".rule");
+
+                less = $this.find(".code pre").html();
+                output = $this.find(".output pre");
+
+                lessParser(less, output);
+            });
+        }
+    }
+}());
+
+css3lessplease.init();
